17bb194a8656948834924ed1931337c2f94da4b9
sergio <soporte@plataformacocrear.com>
2022-05-10 21:51:07 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name':'Odoo Academy',
+    
+    'sumary': """Academy app to manage Training """,
+    
+    'description':"""
+        Academy module to manage training:
+        -courses
+        -sessions
+        -attendees    
+    """,
+    
+    'author': 'Odoo',
+    
+    'website':'https://www.odoo.com',
+    
+    'category':'training',
+    'version':'0.1',
+    
+    'depends':['base'],
+    
+    'data':[
+        
+    ],
+    
+    'demo':[
+        
+    ],
+}
