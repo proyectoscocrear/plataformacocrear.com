482bfdc5c9b3e57d75e323bff3f2eee23882e0a4
sergio <soporte@plataformacocrear>
2022-05-17 21:15:18 +0000

    [SS] dia 6

--- odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -25,6 +25,8 @@
         'security/academy_security.xml',
         'security/ir.model.access.csv',
         'views/academy_menuitems.xml',
+        'views/course_views.xml',
+        'views/session_views.xml'
     ],
     
     'demo':[
