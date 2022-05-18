34ef41b1d5ec4c60e36c3805da0aae2c96c25ae9
sergio <soporte@plataformacocrear>
2022-05-18 19:56:10 +0000

    [SERGIO] dia seis

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -28,7 +28,7 @@ class Course(models.Model):
     
     total_price = fields.Float(string='Total Price', readonly=True)
     
-    session_ids = fields.One2many(comodel_name='academy.sessioon',
+    session_ids = fields.One2many(comodel_name='academy.session',
                                  inverse_name='course_id',
                                  string='Sessions')
     
